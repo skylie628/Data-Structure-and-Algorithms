434a44a311b62bf1f28b6629bd6bc1a2af933660
skylie628 <khuong.dinhbkk17@hcmut.edu.vn>
2023-09-19 21:07:16 +0800

    feat: H-index

---
+++ H-index.js
@@ -0,0 +1,14 @@
+/*Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.*/
+var hIndex = function (citations) {
+  citations.sort((a, b) => b - a);
+  let rst = 0;
+  let n = citations.length;
+  for (let i = 0; i < n; i++) {
+    if (citations[i] <= i + 1) {
+      return citations[i] < i ? i : citations[i];
+    }
+  }
+  return citations.length;
+};
